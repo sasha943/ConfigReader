3a9dd9ea2f470b4fd5ddf28f5d49b792d5de99dc
Sasha943 <MaslikSASHA1@gmail.com>
2018-05-20 21:08:18 +0300

    add logging

--- src/main/java/jackson/JacksonYamlMapper.java
+++ src/main/java/jackson/JacksonYamlMapper.java
@@ -3,10 +3,15 @@ package jackson;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import models.ServerProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.net.URL;
 
 public class JacksonYamlMapper {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JacksonYamlMapper.class);
 
     private ServerProperties serverProperties;
 
@@ -15,13 +20,16 @@ public class JacksonYamlMapper {
     }
 
     private ServerProperties getServerProperties(){
+        LOG.info("Get server properties");
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         ServerProperties serverProperties = null;
         try {
-            File yamlFile = new File("/home/ubuntu/IdeaProjects/ConfigReader/src/main/resources/application.yaml");
+            URL url = getClass().getClassLoader().getResource("application.yaml");
+            LOG.info("Url to file is: {}", url);
+            File yamlFile = new File(url.getPath());
             serverProperties = mapper.readValue(yamlFile, ServerProperties.class);
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.warn("can not map file to object: {}", e.getMessage());
         }
         return serverProperties;
     }
